cf362b78c51fd254396883650b5423a00ee3e331
Omar Blas Lopez <oblas@avances.com.pe>
2022-07-13 03:14:12 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        Tiktok Clone
+      </main>
+    </div>
+  )
+}
+
+export default App
